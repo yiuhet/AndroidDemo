53a45b493c6dc9728959abbbc52a36af09c3d83d
yiuhet <yiuhet@qq.com>
2019-10-23 10:11:37 +0800

    mvp模版

---
+++ mvpdemo/src/main/java/com/liang/mvpdemo/mvp/model/api/Api.java
@@ -0,0 +1,14 @@
+package com.liang.mvpdemo.mvp.model.api;
+
+/**
+ * ================================================
+ * 存放一些与 API 有关的东西,如请求地址,请求码等
+ * <p>
+ * Created by MVPArmsTemplate on 10/23/2019 09:51
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public interface Api {
+    String APP_DOMAIN = "https://api.github.com";
+}
